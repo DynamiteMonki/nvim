16c01bbee1e50c563b96d47fab39b442db9058a0
Vivek <thistovivek@gmail.com>
2025-08-20 17:15:03 +0530

    Removed DAP

--- lua/plugins/debug.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
-    "jay-babu/mason-nvim-dap.nvim",
-    'theHamsta/nvim-dap-virtual-text',
-  },
-}
